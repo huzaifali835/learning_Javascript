0cd547587ad45c219cd0122a139af66cac21c199
Huzaifa Ali <huzaifali795@gmail.com>
2024-06-25 12:12:36 +0000

    learning about datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // Treat all JS code as newer version
+
+// alert(3 + 3); In node JS we can use Alert because this can be run in browsers.
+
+let name = "Huzaifa" // String
+let age = 22 // Int
+let isLoggedIn = false // Boolean
+let state; // Undefined
+let user = null
+
+/* Datatypes
+Primitives Datatypes
+Int => 2 power to 53
+bigInt
+boolean => True/False
+string => ""
+null => Standalone Value
+undefined => declared with a value.
+*/
+
+console.log(typeof age);
+console.log(typeof state);
+
+console.log(typeof user);
